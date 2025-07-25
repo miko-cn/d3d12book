fc6ce3fb354a0aa8502aec8bd201fd5e496f10e2
Yumiko <augbubbles@foxmail.com>
2025-07-25 14:24:24 +0800

    Add DXMathTest

---
+++ MyDemos/DirectXMathTest/DirectXMathTest.cpp
@@ -0,0 +1,48 @@
+﻿#include <windows.h>
+#include <DirectXMath.h>
+#include <DirectXPackedVector.h>
+#include <iostream>
+
+using namespace std;
+using namespace DirectX;
+using namespace DirectX::PackedVector;
+
+// 重载"<<"运算符，通过cout输出XMVECTOR
+ostream& XM_CALLCONV operator<<(ostream& os, FXMVECTOR v)
+{
+    XMFLOAT3 dest;
+    XMStoreFloat3(&dest, v);
+
+    os << "(" << dest.x << ", " << dest.y << ", " << dest.z << ")";
+    return os;
+}
+
+void XMVectorCheck()
+{
+    XMVECTOR p = XMVectorZero();
+    XMVECTOR q = XMVectorSplatOne();
+    XMVECTOR u = XMVectorSet(1.0f, 2.0f, 3.0f, 0.0f);
+    XMVECTOR v = XMVectorReplicate(-2.0f);
+    XMVECTOR w = XMVectorSplatZ(u);
+    cout << "p = " << p << endl;
+    cout << "q = " << q << endl;
+    cout << "u = " << u << endl;
+    cout << "v = " << v << endl;
+    cout << "w = " << w << endl;
+}
+
+int main()
+{
+    cout.setf(ios_base::boolalpha);
+
+    // 检查是否支持SSE2指令集 （Pentium4，AMD K8及其后续版本的处理器）
+    if (!XMVerifyCPUSupport())
+    {
+        cout << "directx math not supported" << endl;
+        return 0;
+    }
+
+    XMVectorCheck();
+
+    return 0;
+}
